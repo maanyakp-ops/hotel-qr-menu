f6039e2cf81a3feda85f23928eb6ef9b8f0acbe7
maanya kumar parmar <maanyakp@gmail.com>
2026-05-22 18:35:19 +0530

    fix service worker caching

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,18 @@
 const CACHE_NAME = "hotel-menu-v1"
-const urlsToCache = ["/", "/index.html"]
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  )
+  self.skipWaiting()
+})
+
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.map(key => caches.delete(key)))
+  ))
 })
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  )
+  if (event.request.mode === "navigate") {
+    return
+  }
+  event.respondWith(fetch(event.request))
 })
